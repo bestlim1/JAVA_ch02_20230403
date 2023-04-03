c8586dbeccffa1bd68d03cc25fb9a71637f0c5c7
sigma <sigmund206@hotmail.com>
2023-04-03 16:08:59 +0900

    java chapter02 변수선언 예제 추가

---
+++ src/test002/Hello.java
@@ -0,0 +1,27 @@
+package test002;
+
+public class Hello {
+
+	public static int sum(int n, int m) {
+		return n + m;
+	}
+	
+	public static void main(String[] args) {
+		int i = 20;
+		
+		int s;
+		double a = 1.1;
+		
+		int b = 1;
+		
+		s = sum(i,10);
+		
+		long c;
+		c = 200000000L;
+		
+		byte d = 1;
+		
+		System.out.println(s);
+	}
+	
+}
